6bd7c141ad0f42ef2370596df57bea1557f3adcf
Soumare <lyfidyma@gmail.com>
2024-09-26 13:37:33 +0000

    Add @Service annotation to CategorieService and CommuneService

--- src/main/java/com/carto/sn/service/CategorieServiceImpl.java
+++ src/main/java/com/carto/sn/service/CategorieServiceImpl.java
@@ -3,10 +3,12 @@ package com.carto.sn.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.carto.sn.dao.CategorieRepository;
 import com.carto.sn.entities.Categorie;
 
+@Service
 public class CategorieServiceImpl implements CategorieService{
 	
 	@Autowired
