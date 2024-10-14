37a68a9edf978e7a34e82f13a4e39cc00a10de2d
Soumare <lyfidyma@gmail.com>
2024-10-14 15:41:22 +0000

    Delete ICarto, CartoImpl and CartoController

--- src/main/java/com/carto/sn/controller/PartenaireController.java
+++ src/main/java/com/carto/sn/controller/PartenaireController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -27,6 +29,8 @@ import com.carto.sn.service.TypeService;
 
 import jakarta.validation.Valid;
 
+@Controller
+@PreAuthorize("isAuthenticated()")
 public class PartenaireController {
 	
 	@Autowired
